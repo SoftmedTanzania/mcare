912f98377b229ac4367e20d234289e09fb4f2604
ebundala <ebundala@gmail.com>
2018-11-01 01:34:16 +0300

    migrating to gcp

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ else{
 		  host: process.env.DB_HOST,
 		  user: process.env.DB_USER,
 		  password: process.env.DB_PASS,
-		  database: process.env.DB_DATABASE
+		  database: process.env.DB_NAME
 		}
 }
 //app.use(express.logger());
