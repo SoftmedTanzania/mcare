3bb213f4a3652b0198a3141157e687ce537bd9ab
silasonyango93 <silas.onyango93@gmail.com>
2018-11-01 23:21:10 +0300

    gaga

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ else{
 		  host: process.env.DB_HOST,
 		  user: process.env.DB_USER,
 		  password: process.env.DB_PASS,
-		  database: process.env.DB_DATABASE
+		  database: process.env.DB_DATABASE,
+	 insecureAuth : true
 		}
 }
 //app.use(express.logger());
