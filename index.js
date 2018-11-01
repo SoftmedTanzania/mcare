a23e65fe1ba2ddd90af3405c8e8bda48d16d68c2
ebundala <ebundala@gmail.com>
2018-11-01 12:26:17 +0300

    db con test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+const mysqlx = require('@mysql/xdevapi');
 var express = require('express');
 var app = express();
 var path =require("path");
@@ -8,49 +8,49 @@ if(process.env.DB_USER==null){
 }
 else{
  dbcredentials={
-		  host: process.env.DB_HOST,
-		  user: process.env.DB_USER,
-		  password: process.env.DB_PASS,
-		  database: process.env.DB_NAME
+		  host:"localhost", //process.env.DB_HOST,
+		  user:"root", //process.env.DB_USER,
+		  password:"admin", //process.env.DB_PASS,
+      schema:"mcare", //process.env.DB_NAME,
+      port: 33060,
 		}
 }
-//app.use(express.logger());
-var con; 
-app.use((req,res,next)=>{
-  
- con = mysql.createConnection(dbcredentials);
-	con.on('error',(err) =>{
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      console.log(err);                        // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      //throw err;                                  // server variable configures this)
-    }
-  });
-	console.log("connect middleware");
 
-	next();
+
+
+app.use((req,res,next)=>{
+  mysqlx.getSession(dbcredentials).then((session)=>{
+    if(session!=null){
+      req.dbSession=session;
+      next();
+    }
+
+  }).catch((err)=>{
+    throw err;
+  });
+ 
 });
 
 
 
 
 
-app.get('/',function(request,response){
-
-con.query("SELECT * FROM department_types", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-	response.send(JSON.stringify(result));
-  });
-
-
+app.get('/',function(req,res){
+   if(req.dbSession==null){
+     throw new Error("No database session");
+   }
+  req.dbSession.getSchema('mcare').getTable('department_types')
+                    .select()
+                    .execute(row => {
+                        console.log(row); // ['bar', 42]
+                        res.send(row);
+                    });
 });
 
 
 app.use(function (err, req, res, next) {
   console.error(err.stack)
-  res.status(500).send('Something broke!')
+  res.status(500).send('Something broke!\n'+err.message);
 })
 
 var port = process.env.PORT || 5000;
