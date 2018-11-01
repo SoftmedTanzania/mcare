50a082d5011e0ada67cad5aac44ce9d364c2c412
silasonyango93 <silas.onyango93@gmail.com>
2018-11-02 01:47:18 +0300

    Agreed standard conventions

--- index.js
+++ index.js
@@ -2,31 +2,12 @@ var mysql = require('mysql');
 var express = require('express');
 var app = express();
 var path =require("path");
-var dbcredentials;
-if(process.env.DB_USER==null){
- dbcredentials=require("./dbcredentials.js");
-}
-else{
- dbcredentials={
-		  host: process.env.DB_HOST,
-		  user: process.env.DB_USER,
-		  password: process.env.DB_PASS,
-		  database: process.env.DB_DATABASE,
-	 insecureAuth : true
-		}
-}
-//app.use(express.logger());
 var con; 
+con=require('./common/dbConnect.js'); 
+
+
 app.use((req,res,next)=>{
- con = mysql.createConnection(dbcredentials);
-	con.on('error',(err) =>{
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      console.log(err);                        // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      //throw err;                                  // server variable configures this)
-    }
-  });
+
 	console.log("connect middleware");
 
 	next();
