91ae9b2fe38654c081d9bcb37cdafc0f65fc5d96
silasonyango93 <silas.onyango93@gmail.com>
2018-11-02 20:54:45 +0300

    son/2018-11-02

--- index.js
+++ index.js
@@ -6,52 +6,55 @@ var dbcredentials;
 var PatientController = require('./controllers/patient_management/PatientController.js');
 
 if(process.env.DB_USER==null){
- dbcredentials=require("./dbcredentials.js");
+    dbcredentials=require("./dbcredentials.js");
 }
 else{
- dbcredentials={
-		  host: process.env.DB_HOST,
-		  user: process.env.DB_USER,
-		  password: process.env.DB_PASS,
-		  database: process.env.DB_DATABASE,
-	 insecureAuth : true
-		}
+    dbcredentials={
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE,
+        insecureAuth : true
+    }
 }
 //app.use(express.logger());
-var con; 
+var con;
 app.use((req,res,next)=>{
- con = mysql.createConnection(dbcredentials);
-	con.on('error',(err) =>{
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      console.log(err);                        // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      //throw err;                                  // server variable configures this)
-    }
-  });
-	console.log("connect middleware");
+    con = mysql.createConnection(dbcredentials);
+    con.on('error',(err) =>{
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            console.log(err);                        // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            //throw err;                                  // server variable configures this)
+        }
+    });
+    console.log("connect middleware");
 
-	next();
+    next();
 });
 
 
 
-
-
 app.get('/',function(request,response){
-	var myPatientControllerObject=new PatientController();
-myPatientControllerObject.insertPatient();
+    response.send("Hello!Welcome to mcare");
+
+});
+
+app.get('/patient_registration',function(request,response){
+    var myPatientControllerObject=new PatientController();
+    myPatientControllerObject.insertPatient();
 
 });
 
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
 })
 
 var port = process.env.PORT || 5000;
 
 app.listen(port,function(){
-console.log("Listening on "+port);
+    console.log("Listening on "+port);
 });
--- routes/patient_management/PatientRoutes.js
+++ routes/patient_management/PatientRoutes.js
@@ -1,42 +1,52 @@
-var mysql = require('mysql');
-var express = require('express');
-var app = express();
-var path =require("path");
-var con; 
-con=require('/common/dbConnect.js'); 
-var PatientController = require('/mcare/controllers/patient_management/PatientController.js');
-var myPatientControllerObject;
-module.exports = class PatientRoutes{
-	constructor(){
-    
- }
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const path =require("path");
+var dbcredentials;
+const PatientController = require('../../controllers/patient_management/PatientController.js');
 
-app.use((req,res,next)=>{
+var PatientRoutesModule = (function () {
 
-	console.log("connect middleware");
+    var PatientRoutes = function () {
 
-	next();
-});
+    };
+
+    dbcredentials = {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE,
+        insecureAuth: true
+    }
+
+//app.use(express.logger());
+    var con;
+    PatientRoutes.prototype.app.use((req, res, next) => {
+        con = mysql.createConnection(dbcredentials);
+        con.on('error', (err) => {
+            console.log('db error', err);
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+                console.log(err);                        // lost due to either server restart, or a
+            } else {                                      // connnection idle timeout (the wait_timeout
+                //throw err;                                  // server variable configures this)
+            }
+        });
+        console.log("connect middleware");
+
+        next();
+    });
 
 
+    PatientRoutes.prototype.app.get('/patient_registration', function (request, response) {
+        var myPatientControllerObject = new PatientController();
+        myPatientControllerObject.insertPatient();
 
+    });
 
+    return {
+        PatientRoutes: PatientRoutes
+    }
 
-app.get('/insert_patient',function(request,response){
+}());
 
-myPatientControllerObject=new PatientController();
-	myPatientControllerObject.insertPatient();
-});
-
-
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
-
-var port = process.env.PORT || 5000;
-
-app.listen(port,function(){
-console.log("Listening on "+port);
-});
-}
+module.exports = PatientRoutesModule;
